a90f07978358fc769a7def8a130ac8e05692d1fa
Parpibaev Artur <havofavo@mail.ru>
2018-05-25 18:59:02 +0300

    Revert "-- single test photos init"

--- run_tests.py
+++ run_tests.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 import unittest
 
-
-from util import config
+from tests.AddImageTests import AddImageTests
+from tests.CreateAlbumTests import CreateAlbumTests
+from tests.comments_test import CommentsTest
+from tests.mobile_album_create_test import MobileAlbumCreateTest
+from tests.mobile_album_test import MobileAlbumTest
 
 
 if __name__ == '__main__':
-    os.environ[config.PREFERRED_BROWSER_KEY] = config.PREF_CHROME
-    suite = unittest.TestLoader().discover(config.TEST_DIR)
-    result = unittest.TextTestRunner().run(suite)
-    os.environ[config.PREFERRED_BROWSER_KEY] = config.PREF_FIREFOX
-    suite = unittest.TestLoader().discover(config.TEST_DIR)
-
+    suite = unittest.TestSuite((
+        unittest.makeSuite(MobileAlbumCreateTest),
+        unittest.makeSuite(MobileAlbumTest),
+        unittest.makeSuite(CommentsTest),
+        unittest.makeSuite(CreateAlbumTests),
+        unittest.makeSuite(AddImageTests),
+    ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
